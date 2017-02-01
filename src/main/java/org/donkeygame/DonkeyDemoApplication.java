96d8e3adb8ad5c71581dc8581ab2508997da3914
smcvb <smcvbeelen@gmail.com>
2017-02-01 11:37:52 +0100

    [Donkey Demo] Initialize project
    -Initialize project
    -Add several dependencies

---
+++ src/main/java/org/donkeygame/DonkeyDemoApplication.java
@@ -0,0 +1,14 @@
+package org.donkeygame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DonkeyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonkeyDemoApplication.class, args);
+	}
+
+}
